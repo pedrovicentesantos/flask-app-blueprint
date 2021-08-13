353be7582fb3aed4932250940282f2b532770bd5
Pedro Vicente Santos <pedrovicente@poli.ufrj.br>
2021-08-13 19:27:03 -0300

    Add comments to app file

--- app/app.py
+++ app/app.py
@@ -1,3 +1,6 @@
+'''
+Creates the Flask app
+'''
 from flask import Flask, jsonify
 
 from distance.distance import distance_blueprint
@@ -5,11 +8,20 @@ from utils.utils import load_config
 
 
 def create_app(config_name):
+    '''
+    Uses the config name to load
+    the correct configurations for app
+    '''
     load_config(config_name)
 
     app = Flask(__name__)
+    # Add the blueprint to the application
     app.register_blueprint(distance_blueprint)
 
+    '''
+    Add a handler for when the user
+    tries to access a route that doesn't exist
+    '''
     @app.errorhandler(404)
     def page_not_found(e):
         print(e)
